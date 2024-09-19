7dcfc7c1d7fd2bf3e9dc91694c4c09f3761bc3c9
Kunj Moradiya <165253660+Bobbymkr@users.noreply.github.com>
2024-09-19 13:54:18 +0530

    Add files via upload

---
+++ pr29.java
@@ -0,0 +1,26 @@
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class pr29 {
+    public static void main(String[] args) {
+        
+        String sourceFile = "source.txt";
+        String destinationFile = "destination.txt";
+
+        try (
+            FileInputStream fis = new FileInputStream(sourceFile);
+            FileOutputStream fos = new FileOutputStream(destinationFile)
+        ) {
+            int byteRead;
+            
+            while ((byteRead = fis.read()) != -1) {
+                fos.write(byteRead);
+            }
+            System.out.println("File copied successfully!");
+        } catch (IOException e) {
+            System.out.println("An error occurred during file copying:");
+            e.printStackTrace();
+        }
+    }
+}
